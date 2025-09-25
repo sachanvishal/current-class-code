270fc6870ee935a5367e70b1a2793d9f76705fc8
sachanvishal <30946665+sachanvishal@users.noreply.github.com>
2025-09-26 01:18:21 +0530

    Update SeatInShowRepository.java

--- 23rd Aug 2023/bms_fair_begin/src/main/java/com/example/bms_fair_begin/repositories/SeatInShowRepository.java
+++ 23rd Aug 2023/bms_fair_begin/src/main/java/com/example/bms_fair_begin/repositories/SeatInShowRepository.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+//Long is for id can use it also
 public interface SeatInShowRepository extends JpaRepository<SeatInShow, Long> {
+    //Hibernate would write the query for you or it would provide the implementation for methods which you overwrite here.
     @Override
     List<SeatInShow> findAllById(Iterable<Long> longs);
 
